5b77897628dc69331dabd3e767d689f31a76db1f
OVAnechka <ocrio.box@gmail.com>
2017-01-17 10:02:43 +0300

    Пишем маркеры, и начинаем читать данные из фифо, распределяем, но неверно отдаём наружу.

--- filler.v
+++ filler.v
@@ -33,7 +33,7 @@ assign	mark[14]	=	{ nM[8:0],	nB[12:11]	};
 assign	mark[15]	=	{ nB[10:0] 					};
 
 wire				e, f;
-wire	[16:0]	out;
+wire	[16:0]	fifoOut;
 reg	[2:0]		readyReg;
 wire				readyFront;
 reg				oldSwitch;
@@ -52,10 +52,9 @@ assign	readyFront		=	(!readyReg[2] & readyReg[1]);
 assign	bufferChanged	=	(oldSwitch != bufSwitch) ? 1'b1 : 1'b0;
 
 // Takes 2 clocks to set output, use 4 to be sure
-digitalFIFO fifo( .clock(clk), .data(word), .rdreq(fifoRead), .wrreq(readyFront), .empty(e), .full(f), .q(out), .usedw(fifoUsed));
+digitalFIFO fifo( .clock(clk), .data(word), .rdreq(fifoRead), .wrreq(readyFront), .empty(e), .full(f), .q(fifoOut), .usedw(fifoUsed));
 
 reg	[3:0]		rSequence;
-reg	[2:0]		pBegin, pEnd;
 reg	[7:0]		cntSubStream;
 reg	[2:0]		cntMarker;
 reg	[2:0]		state;
@@ -63,10 +62,10 @@ reg				WRITE_MARKER = 3'd0;
 reg				WRITE_DATA = 3'd1;
 reg				CHECK_BUFFER = 3'd2;
 reg				sender;
-wire				act;
 reg				enable;
-
-assign act = ((fifoUsed > 320) && (fifoUsed < 1020)) ? 1'b1 : 1'b0;
+reg	[175:0]	toWriteBuffer;
+reg	[3:0]		pWR;
+reg	[3:0]		pRD;
 
 always@(posedge clk or negedge reset) begin
 	if (~reset) begin
@@ -74,17 +73,19 @@ always@(posedge clk or negedge reset) begin
 		outWREN <= 1'b0;
 		outWADR <= 10'b1;
 		rSequence <= 4'b0;
-		pBegin <= 0;
-		pEnd <= 10;
 		cntSubStream <= 8'b0;
 		cntMarker <= 3'b0;
 		state <= 3'b0;
 		sender <= 1'b0;
 		enable <= 1'b0;
+		fifoRead <= 1'b0;
+		pWR <= 4'b0;
+		pRD <= 4'b0;
+		toWriteBuffer <= 176'b0;
 	end else begin
-		if (fifoUsed == 1000) enable <= 1'b1;			// once we get 1000 words - enable the following scheme
+		if ((fifoUsed > 640) && (bufferChanged)) enable <= 1'b1;			// once we get 1000 words - enable the following scheme
 
-		if (enable && act) begin
+		if (enable) begin
 			case (state)
 				WRITE_MARKER: begin
 					rSequence <= rSequence + 1'b1;
@@ -96,11 +97,12 @@ always@(posedge clk or negedge reset) begin
 						3: begin
 							cntMarker <= cntMarker + 1'b1;
 							outWREN <= 1'b0;
-							if((outWADR[1:0] == 2'd3))
-								outWADR <= outWADR + 2'd2;
-							else
-								outWADR <= outWADR + 1'b1;
 							state <= CHECK_BUFFER;
+							if((outWADR[1:0] == 2'd3)) begin
+								outWADR <= outWADR + 2'd2;
+							end else begin
+								outWADR <= outWADR + 1'b1;
+							end
 						end
 						4: begin
 							rSequence <= 4'b0;
@@ -132,12 +134,64 @@ always@(posedge clk or negedge reset) begin
 					rSequence <= rSequence + 1'b1;
 					case(rSequence)
 						0: begin
-						//	fifoRead <= 1'b1;
+							//read 11 words and write 16 words at a time
+							fifoRead <= 1'b1;
 						end
+						1: fifoRead <= 1'b0;
 						3: begin
-							
+							case (pWR)
+								0: toWriteBuffer[175 : 160] <= fifoOut;
+								1: toWriteBuffer[159 : 144] <= fifoOut;
+								2: toWriteBuffer[143 : 128] <= fifoOut;
+								3: toWriteBuffer[127 : 112] <= fifoOut;
+								4: toWriteBuffer[111 : 96] <= fifoOut;
+								5: toWriteBuffer[95 : 80] <= fifoOut;
+								6: toWriteBuffer[79 : 64] <= fifoOut;
+								7: toWriteBuffer[63 : 48] <= fifoOut;
+								8: toWriteBuffer[47 : 32] <= fifoOut;
+								9: toWriteBuffer[31 : 16] <= fifoOut;
+								10: toWriteBuffer[15 : 0] <= fifoOut;
+							endcase
+							pWR <= pWR + 1'b1;
+							if (pWR < 10) begin
+								rSequence <= 4'b0;
+							end else begin
+								rSequence <= 4'd4;
+								pWR <= 1'b0;
+							end
 						end
-						default: begin
+						4: begin
+							pRD <= pRD + 1'b1;
+							case(pRD)
+								0: outWDAT <= { 1'b0, toWriteBuffer[175: 165] };
+								1: outWDAT <= { 1'b0, toWriteBuffer[164: 154] };
+								2: outWDAT <= { 1'b0, toWriteBuffer[153: 143] };
+								3: outWDAT <= { 1'b0, toWriteBuffer[142: 132] };
+								4: outWDAT <= { 1'b0, toWriteBuffer[131: 121] };
+								5: outWDAT <= { 1'b0, toWriteBuffer[120: 110] };
+								6: outWDAT <= { 1'b0, toWriteBuffer[109: 99] };
+								7: outWDAT <= { 1'b0, toWriteBuffer[98: 88] };
+								8: outWDAT <= { 1'b0, toWriteBuffer[87: 77] };
+								9: outWDAT <= { 1'b0, toWriteBuffer[76: 66] };
+								10: outWDAT <= { 1'b0, toWriteBuffer[65: 55] };
+								11: outWDAT <= { 1'b0, toWriteBuffer[54: 44] };
+								12: outWDAT <= { 1'b0, toWriteBuffer[43: 33] };
+								13: outWDAT <= { 1'b0, toWriteBuffer[32: 22] };
+								14: outWDAT <= { 1'b0, toWriteBuffer[21: 11] };
+								15: outWDAT <= { 1'b0, toWriteBuffer[10: 0] };
+							endcase
+						end
+						5: outWREN <= 1'b1;
+						7: begin
+							outWREN <= 1'b0;
+							sender <= 1'b0;
+							state <= CHECK_BUFFER;
+							if((outWADR[1:0] == 2'd3)) begin
+								outWADR <= outWADR + 2'd2;
+							end else begin
+								outWADR <= outWADR + 1'b1;
+							end
+							if (pRD > 0) rSequence <= 4'd4;
 						end
 					endcase
 				end
